dedb3664b2b17c9950cbd16fb80485c999c2ee7d
Kuenane <lebohang.kuenane44@gmail.com>
2022-08-08 05:49:40 +0200

    second commit

--- src/Row.tsx
+++ src/Row.tsx
@@ -2,41 +2,139 @@ import { type } from 'os';
 import { title } from 'process';
 import React, {useState, useEffect} from 'react';
 import axios from './axios';
+import _ from 'lodash';
 
-/*type Prop = {
+const base_url:string = 'https://image.tmdb.org/t/p/original';
+
+type Props = {
     title: string;
     fetchUrl: string;
+    isLargeRow?: boolean;
+  };
+  
+  export interface Welcome6 {
+    took:      number;
+    timed_out: boolean;
+    _shards:   Shards;
+    hits:      Hits;
 }
-*/
 
-function Row({title, fetchUrl}:{title: any, fetchUrl:any}): JSX.Element {   
-    const [movies, setMovies] = useState([]);
+export interface Shards {
+    total:      number;
+    successful: number;
+    skipped:    number;
+    failed:     number;
+}
 
+export interface Hits {
+    total:     Total;
+    max_score: number;
+    hits:      Hit[];
+}
+
+export interface Hit {
+    _index:  Index;
+    _type:   Type;
+    _id:     string;
+    _score:  number;
+    _source: Movie;
+}
+
+export enum Index {
+    ContentIndex = "content-index",
+}
+
+export interface Movie{
+    metadata:    Metadata;
+    year:        string;
+    rating:      number;
+    description: string;
+    episode:     number;
+    type:        SourceType;
+    duration:    number;
+    objectKey:   string;
+    genre:       Genre[];
+    name:        string;
+    guid:        string;
+    season:      number;
+    order:       number;
+    parents:     string[];
+}
+
+export enum Genre {
+    Adventure = "Adventure",
+    Drama = "Drama",
+    Fantasy = "Fantasy",
+}
+
+export interface Metadata {
+    images: Image[];
+}
+
+export interface Image {
+    type: ImageType;
+    url:  string;
+}
+
+export enum ImageType {
+    Poster = "poster",
+}
+
+export enum SourceType {
+    Episode = "episode",
+}
+
+export enum Type {
+    Doc = "_doc",
+}
+
+export interface Total {
+    value:    number;
+    relation: string;
+}
+
+
+function Row({title, fetchUrl}:Props): JSX.Element {   
+    const [movies, setMovies] = useState<Movie[]>([]);
     // A snippet of code that runs based on a specific condition    
     useEffect(() => {
         // if [], run once when row loads and don't run again
         // While the data is loading, do something
-
         async function fetchData() {
-            const request = await axios.get(fetchUrl);
-            setMovies(request.data);
-            //console.table(request.data)
-            console.log(request);
-            return request;
-            
-            
+            const fetchRequest = await axios.get(fetchUrl);
+            setMovies(fetchRequest.data);
+            return fetchRequest;
         }
         fetchData();
     }, [fetchUrl]);
     
-
-    console.log(movies);
-
+    //console.log(movies);
+    if (!movies) return null as any;
     return (
-        <div>
+        <>
+        <div className='row'>
             <h2>{title}</h2>
+            <div className='row__posters'>
+                {movies.map((movie, i) => (
+                    <><div>
+
+                        <h5>{movie.name}</h5>
+                        <p>{movie.metadata.images[0].url}</p>
+                    </div><>
+
+                           
+                            {/* {movie.metadata.images && movie.metadata.images.map((image, i) => (
+        <img key={i} src={image.url} />
+
+    ))} */}
+                        </></>
+                        
+                ))}
+            </div>        
         </div>
-    ) 
+        </>
+        
+    ); 
 }
 
 export default Row;
